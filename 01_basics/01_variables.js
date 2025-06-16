08a80de58636407bd40286bd02c5387b7e2bc550
Dua613 <baluchdua1@gmail.com>
2025-06-16 07:26:29 +0000

    my javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1445533
+let accountEmail = "baluch@google.com"
+var accountpassword = "237813364"
+accountcity = "karachi"
+let accountState;  //undefined
+
+//const accountId = 5324538 //not allowed
+
+//let accountEmail = "duadua@google.com" //if take 2 values it shows errors
+
+var accountpassword = "11111"
+accountcity = "turbat"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountpassword, accountcity])
+
