3324212abdce9d7d2e6d221ce8f68147a1ff304b
Dua613 <baluchdua1@gmail.com>
2025-06-17 05:33:27 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let got = "shari"
+
+//console.log(typeof got);
+//console.log(typeof(got));
+
+let valueInNumber = Number(got)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber) //Not a Number
+
+//STRING INTO NUMBER
+  // "33" => 33
+  // "33abc" => NaN
+  // "true" => 1, "false"=> 2
+  //null => 0
+
+
+let loggedin = "shari";
+
+let booleanloggedin = Boolean(loggedin);
+//console.log(booleanloggedin)
+
+  //SO THE OUTCOME IS
+    // 1 => true,  0 => false
+    // "" => false,  "shari" => true
+
+let numberis = 10
+
+let stringNumber = String(numberis)
+console.log(stringNumber);
+console.log(typeof stringNumber)
