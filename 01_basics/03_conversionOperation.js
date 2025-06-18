adfc2ef7f0d6500baf2ea2416409e5e7ce1fdc99
Dua613 <baluchdua1@gmail.com>
2025-06-18 04:33:44 +0000

    comparisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,5 +26,52 @@ let booleanloggedin = Boolean(loggedin);
 let numberis = 10
 
 let stringNumber = String(numberis)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber)
+
+
+//**********************  OPERATIONS  ***************************
+
+let value = 4;
+let negvalue = -value;
+//console.log(negvalue)
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%2);*/
+
+let value1 = "hello";
+let value2 = " shari";
+let value3 = value1 + value2;
+//console.log(value3)
+
+/*console.log("2" + 1)
+console.log(1 + "2")
+console.log("1" + 1 +1)
+console.log(1  + 2 + "2")*/
+
+//console.log(3+4*5%6)
+
+let num1, num2, num3
+mum1 = num2 = num3 = 2+2  // right but donot write like this!
+
+let shari=2
+shari++;
+//console.log(shari)
+
+//************PREFIX*************
+let me = 3;
+const you = ++me;
+console.log(you)
+console.log(me)
+// me is 4; you is 4
+
+//***********POSTFIX************** 
+let m = 3;
+const y = m++;
+console.log(m)
+console.log(y)
+// m is 4; y is 3
