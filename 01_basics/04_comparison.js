b577e5af4b776ba6169c539614aaa144fd2a63f5
Dua613 <baluchdua1@gmail.com>
2025-06-20 06:14:18 +0000

    comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,1 +1,36 @@
-console.log()
+// console.log(3 < 1);
+// console.log(2 <=1);
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+ 
+// NOTE! these comaprison are not much important as they are easy to write and understand
+//           we cant write ...console.log(3 = 1);
+
+
+
+// console.log("2" > 1);
+//   NOTE! this convert string into number automatically
+
+
+// console.log(null == 0)
+//     console.log(null != 0)
+//         console.log(null > 0)
+//             console.log(null >= 0)
+
+// NOTE!  here conversion is happening in some and not in some
+   //avoid such comparison
+
+
+//    console.log(undefined > 0)
+//    console.log(undefined < 0)
+//    console.log(undefined == 0)
+
+//    NOTE!  avoid such comparison
+
+
+// STRICT COMPARISON
+//nsole.log("2"===2)
+
+//TE!  doesnot convert diferent datatypes.. checks datatypes
