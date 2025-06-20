ed635ba9874376539aa60cbd8733f8ffca915699
Dua613 <baluchdua1@gmail.com>
2025-06-20 06:47:25 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,49 @@
+// PRIMITIVE
+// 7 types : String, Number, Boolean, Null, undefined, 
+// Symbol(use for making value unique), BigInt
+
+
+
+// const score = 100                  //number
+// const scoreValue = 100.3              //number
+
+// const isLoggedIn = false              //boolean
+// const outsideTemp = null              //null
+// let userEmail;                           //undefined
+
+// const id = Symbol('123')                   //symbol
+// const anotherId = Symbol('123')            //symbol
+// console.log(id === anotherId);
+
+//  const bigNumber = 3456543576654356754n           //bigInt
+
+
+
+// REFERENCE (Non primitive)
+// Array, Objects, Functions
+
+
+//const heros = ["shaktiman", "naagraj", "doga"];             //Array
+/*let myObj = {
+    name: "hitesh",
+    age: 22,
+}               */                                           //Object
+
+/*const myFunction = function(){
+    console.log("Hello world");
+}            */                                               //Function
+
+//console.log(typeof anotherId);
+
+
+
+ // https://262.ecma-international.org/5.1/#sec-11.4.3       datatypes link
+
+
+
+                               // JAVASCRIPT TYPED LANGUAGE
+ /*javaScript is a dynamically typed language. This means that the type of a variable
+ is checked during runtime (when the code is executed), rather than at compile time.
+ In JavaScript, you don't need to declare the data type of a variable when you create it;
+ the interpreter automatically determines the type based on the value assigned to it.*/ 
+
