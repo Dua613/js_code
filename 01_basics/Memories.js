25f983033244acb20c8749f0010344a937e94229
Dua613 <baluchdua1@gmail.com>
2025-06-21 07:22:28 +0000

    memories

--- 01_basics/Memories.js
+++ 01_basics/Memories.js
@@ -1,3 +1,27 @@
 //----------TWO TYPES OF MEMORY--------------
-  
 
+// 1.Stack (Primitive datatypes comes in it)     2.Heap (Non Primitive/Reference datatypes comes in it)
+  // this gives a copy                              this gives reference (original value)
+  // changes become in a copy                       changes become in original 
+
+
+  //**************STACK**************** */
+  let myYoutubename = "shari.com"
+
+  let anothername = myYoutubename
+  anothername = "shari baluch"
+
+ console.log(myYoutubename);
+ console.log(anothername);
+
+ //*******************HEAP******************** */
+   let userOne = {
+        email: "user@google.com",
+          upi: "user@ybl"
+         }
+
+   let userTwo = userOne
+   userTwo.email = "shari@google.com"
+
+     console.log(userOne.email);
+     console.log(userTwo.email);
