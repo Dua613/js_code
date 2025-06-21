ef68b2510b4f7dae72ed26be37c7f335827361b0
Dua613 <baluchdua1@gmail.com>
2025-06-21 12:23:17 +0000

    dates and times

---
+++ 01_basics/07_dates_and_time.js
@@ -0,0 +1,39 @@
+let mydate = new Date()
+
+/*console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleTimeString());*/
+
+//let mycreateddate = new Date(25, 2, 21)
+//let mycreateddate = new Date(25 , 2 , 21, 5, 1)
+let mycreateddate = new Date("06-21-2005")
+
+console.log(mycreateddate);
+console.log(mycreateddate.toDateString());
+console.log(mycreateddate.toLocaleString());
+
+
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+//NOTE!  didnt got whats date.now
+
+
+let newdate = new Date()
+
+console.log(newdate);
+console.log(newdate.getDay());
+console.log(newdate.getMonth() + 1);
+//NOTE!  as it takes from 0 by default
+console.log(newdate.getHours());
+////console.log(newdate.getMonth(.toString()));
+
+
