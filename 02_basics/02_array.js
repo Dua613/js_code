cb62c5781f317a32589b8bb9c306e8cb650a2186
Dua613 <baluchdua1@gmail.com>
2025-06-23 13:38:36 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,1 +1,16 @@
-l
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+//console.log(marvel_heros);
+
+marvel_heros.push(dc_heros)              //it merg them
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[2]);      
+// console.log(marvel_heros[3][2]);          //in 3rd index it takes 3 names of dc_heros as a one array 
+                                                 //we can use this but it isnt a good syntax
+
+                                                 
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
