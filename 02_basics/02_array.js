b12f4edceb93363f412aa231acce10c6f5b8082e
Dua613 <baluchdua1@gmail.com>
2025-06-23 16:53:02 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -3,14 +3,25 @@ const dc_heros = ["superman", "flash", "batman"]
 
 //console.log(marvel_heros);
 
-marvel_heros.push(dc_heros)              //it merg them
+
+          //**************.PUSH************ */
+
+//marvel_heros.push(dc_heros)         //it merg them but second arrays names are combined as one array
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[2]);      
 // console.log(marvel_heros[3][2]);          //in 3rd index it takes 3 names of dc_heros as a one array 
                                                  //we can use this but it isnt a good syntax
 
-                                                 
+             //************** */.CONCAT*************                                    
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+ const allHeros = marvel_heros.concat(dc_heros)   //it merg them and take all names as separate arrays
+ console.log(allHeros);
+
+
+ //**************spread************ */
+
+ const new_allheros = [...marvel_heros, ...dc_heros]   //same as concat but more preferred 
+                                                     //because it can take as many as many we want
+ console.log(new_allheros);                           //and concat
+
